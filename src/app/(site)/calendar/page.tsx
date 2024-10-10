2672b4a8501632e5a57503f4a9879550ef1d7704
Nico <judesnicolas@gmail.com>
2024-10-10 16:49:53 -0400

    test Notification si no session

--- src/app/(site)/calendar/page.tsx
+++ src/app/(site)/calendar/page.tsx
@@ -1,13 +1,25 @@
+import ToasterContext from "@/app/api/contex/ToasetContex";
 import Breadcrumb from "@/components/Common/Breadcrumb";
-import { Metadata } from "next";
+import { authOptions } from "@/utils/auth"; // Chemin vers tes options NextAuth
+import { getServerSession } from "next-auth/next";
 
 export const metadata: Metadata = {
-  title:
-    "Pricing Page | Play SaaS Starter Kit and Boilerplate for Next.js",
+  title: "Pricing Page | Play SaaS Starter Kit and Boilerplate for Next.js",
   description: "This is pricing page description",
 };
 
-const CalendarPage = () => {
+const CalendarPage = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    // Si l'utilisateur n'est pas connecté, on affiche un toaster et on redirige
+    return (
+      <>
+        <ToasterContext msg="Vous devez être connecté pour accéder à cette page." color="red" />
+      </>
+    );
+  }
+
   return (
     <>
       <Breadcrumb pageName="Calendar" />
--- src/app/api/contex/ToasetContex.tsx
+++ src/app/api/contex/ToasetContex.tsx
@@ -1,8 +1,29 @@
-"use client";
+'use client';
 
-import { Toaster } from "react-hot-toast";
+import { useEffect } from 'react';
+import { toast, Toaster } from 'react-hot-toast';
 
-const ToasterContext = () => {
+const ToasterContext = (
+  {
+    msg,
+    color,
+  }: {
+    msg: string;
+    color?: string;
+  }
+) => {
+
+  useEffect(() => {
+    if (msg) {
+      toast(msg, {
+        style: {
+          backgroundColor: color || '#fff', // Couleur personnalisée
+          color: color ? '#fff' : '#000', // Si color est défini, texte blanc
+        },
+      });
+    }
+  }, [msg, color]);
+
   return (
     <div className="z-[99999]">
       <Toaster />
